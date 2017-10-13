d138113b40588192bc4dc22cb0e7f39a5b07ef53
Jose Manuel Lopez Martin <josemlm93@gmail.com>
2017-10-13 08:56:19 +0200

    Modificado ejercicio1.java

--- Tema3/Ejercicio1.java
+++ Tema3/Ejercicio1.java
@@ -1,9 +1,9 @@
 /**
-* Lectura de datos desde teclado 
-* Ejercicio 1 Tema 3
-* Realiza un programa que pida dos números y que luego muestre el resultado
-* de su multiplicación. 
-* @author Jose Manuel Lopez Martin
+ * Lectura de datos desde teclado 
+ * Ejercicio 1 Tema 3
+ * Realiza un programa que pida dos números y que luego muestre el resultado
+ * de su multiplicación. 
+ * @author Jose Manuel Lopez Martin
 */
 
 public class Ejercicio1 {
@@ -13,9 +13,10 @@ public class Ejercicio1 {
     System.out.println(" dos numeros enteros.");
     System.out.print("Por favor, introduce un número: ");
     int primerNumero = Integer.parseInt(System.console().readLine());
-     /**esta linea convierte
-    el numero introducido que en principio es una cadena de caracteres
-    * en un numero entero */
+/** esta linea convierte
+ * el numero introducido que en principio es una cadena de caracteres
+ * en un numero entero 
+ */
 
     System.out.print("introduce otro, por favor: ");
     int segundoNumero = Integer.parseInt(System.console().readLine());
