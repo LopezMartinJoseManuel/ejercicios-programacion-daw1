815321340552dfa2ee55be13ff6e5d0c6d7728b6
Jose Manuel Lopez Martin <josemlm93@gmail.com>
2017-10-24 17:18:59 +0200

    Actualizado Ejercicio 23

--- Tema4/Ejercicio23.java
+++ Tema4/Ejercicio23.java
@@ -7,53 +7,64 @@ public class Ejercicio23 {
   public static void main(String[] args) {
 
   System.out.print("Programa que calcula el precio final de producto");
-  System.out.println("segun su base imponible");
+  System.out.println(" segun su base imponible");
   System.out.print("Introduzca la base imponible: ");
   double base = Double.parseDouble(System.console().readLine());
-  System.out.print("Introduzca el tipo de IVA (general, reducido o superreducido");
+  System.out.print("Introduzca el tipo de IVA (general, reducido o superreducido): ");
   String iva = System.console().readLine().toLowerCase(); 
-  System.out.print("Introduzca codigo promocional(nopro, mitad, meno5 o 5porc: ");
+  System.out.print("Introduzca codigo promocional(nopro, mitad, meno5 o 5porc): ");
   String codigo = System.console().readLine().toLowerCase();
+  
   int tipoIva = 0;
+  double ivaCalc = 0;
+  double calcCodi = 0;
+  String tipoCodigo = "";
+  
+
   switch (iva){
     case "general":
-      double IvaCalc = base * 0.21;
+       ivaCalc = base * 0.21;
        tipoIva = 21;
       break;
     case "reducido":
-      IvaCalc = base * 0.10;
+      ivaCalc = base * 0.10;
       tipoIva = 10;
       break;
     case "superreducido":
-      IvaCalc = base * 0.4;
+      ivaCalc = base * 0.4;
       tipoIva = 4;
       break;
     default:
-    System.out.print("Iva incorrecto");
+    System.out.print("\nIva incorrecto");
     }
+  double precioIva = base + ivaCalc;
+  
   switch (codigo){
     case "nopro":
-      double baseNo = base;
-      String tipoCodigo = "base";
+      calcCodi = 0;
+      tipoCodigo = "nopro";
       break;
     case "mitad":
-      double baseMitad =  base / 2;
+      calcCodi =  precioIva / 2;
       tipoCodigo = "mitad";
       break;
     case "meno5":
-      double baseMenos5 = base - 5;
+      calcCodi = 5;
       tipoCodigo = "meno5";
       break;
     case "5porc":
-      double base5porc = base * 0.5;
+      calcCodi = (precioIva * 5) /100;
       tipoCodigo = "5porc";
+      break;
      default:
-     System.out.print("Codigo Incorrecto"); 
+     System.out.print("\nCodigo Incorrecto"); 
     }
-    
-  System.out.print("Base imponible     " + base);
-  System.out.println("Tipo de iva:" + tipoIva);
-
-
+  double total = precioIva - calcCodi;
+  
+  System.out.println("Base imponible        " + base);
+  System.out.printf("Tipo de iva(%2d%%)      %.2f\n", tipoIva, ivaCalc);
+  System.out.println("Precio con Iva        " + precioIva);
+  System.out.printf("Cód. promo. (%s): -%.2f\n", tipoCodigo, calcCodi);
+  System.out.print("TOTAL	              " + total);
 }
 }
