91031f853d6568de68f6f90afdae8f5452e016d4
Jose Manuel Lopez Martin <josemlm93@gmail.com>
2017-10-31 12:00:03 +0100

    Me encantan las piramides

--- Tema5/Ejercicio19.java
+++ Tema5/Ejercicio19.java
@@ -1,1 +1,31 @@
+/**
+ * Ejercicio 19
+ * Realiza un programa que pinte una pirámide por pantalla. La altura se debe
+ * pedir por teclado. El carácter con el que se pinta la pirámide también se debe
+ * pedir por teclado.
+ *
+ * @author José Manuel López Martín
+ */
+import java.util.Scanner;
+
+public class Ejercicio19 {
+  public static void main(String[] args) {
+  Scanner s = new Scanner(System.in);
+  
+  System.out.println("Programa que pinta una pirámide");
+  System.out.print("Introduzca la altura: ");  
+  int altura = Integer.parseInt(System.console().readLine());
+  System.out.print("Introduzca el caracter: ");  
+  String caracter = s.next();
+  int espacios = altura -1;
+  int i = 1;
+  int alturaini = 1;
+  
+  while(alturaini <= altura){
+    System.out.println("*");
+    }
+  altura++;
+  }
+}
+
 
